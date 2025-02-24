51da5e279afe546f244e44283c3034881c9bff25
Roel Castro <rcastro_5450@stu.southtexascollege.edu>
2025-02-23 20:44:05 -0600

    Finished Navagation Menu

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from 'vue'
-import MainCards from '@/components/MainCards.vue';
+  import { ref } from 'vue'
+  import MainCards from '@/components/MainCards.vue';
 </script>
 
 <template>
